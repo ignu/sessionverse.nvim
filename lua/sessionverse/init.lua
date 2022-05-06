82e944749a450da6dc17079de3ac8ee99bab10db
Len Smith <len@barrison.com>
2022-05-05 22:55:00 -0700

    Open git files in tabs

--- lua/sessionverse/init.lua
+++ lua/sessionverse/init.lua
@@ -1,14 +1,34 @@
-print("Loaded SessionVerse 🚀")
-local neogit = require("neogit")
 local cli = require("neogit.lib.git.cli")
 local parser = require("neogit.buffers.commit_view.parsing")
 
 local M = {}
 
+local split_limit = 4
+
+local getTableSize = function(t)
+	local count = 0
+	for _, __ in pairs(t) do
+		count = count + 1
+	end
+	return count
+end
+
+M.getDirtyFiles = function()
+	local rv = {}
+	rv.files = parser.parse_commit_overview(cli.show.stat.oneline.call_sync()).files
+	rv.count = getTableSize(rv.files)
+	return rv
+end
+
 M.openDirtyFiles = function()
-	local git_info = parser.parse_commit_overview(cli.show.stat.oneline.call_sync())
+	local git_info = M.getDirtyFiles()
+
+	local cmd
+	if split_limit > git_info.count then
+		cmd = "vs"
+	end
 	for _, file in pairs(git_info.files) do
-		vim.api.nvim_command("tabe " .. file.path)
+		vim.api.nvim_command(cmd .. " " .. file.path)
 	end
 end
 
