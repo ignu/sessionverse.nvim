b4ad052f86835aa049c62302ece4184f6f1a97b3
Len Smith <len@barrison.com>
2022-05-05 23:35:47 -0700

    Open dirty files first, then last diff's files

--- lua/sessionverse/init.lua
+++ lua/sessionverse/init.lua
@@ -1,3 +1,4 @@
+local status = require("neogit.status")
 local cli = require("neogit.lib.git.cli")
 local parser = require("neogit.buffers.commit_view.parsing")
 
@@ -7,7 +8,7 @@ local split_limit = 4
 
 local getTableSize = function(t)
 	local count = 0
-	for _, __ in pairs(t) do
+	for _ in pairs(t) do
 		count = count + 1
 	end
 	return count
@@ -15,9 +16,29 @@ end
 
 M.getDirtyFiles = function()
 	local rv = {}
+	rv.files = {}
+	for i, f in ipairs(status.repo.unstaged.items) do
+		rv.files[i] = { path = f.name }
+	end
+
+	rv.count = getTableSize(rv.files)
+	return rv
+end
+
+M.getLastCommitFiles = function()
+	local rv = {}
 	rv.files = parser.parse_commit_overview(cli.show.stat.oneline.call_sync()).files
 	rv.count = getTableSize(rv.files)
 	return rv
+end
+
+M.getRelevantFiles = function()
+	local files = M.getDirtyFiles()
+	if files.count > 0 then
+		return files
+	end
+
+	return M.getLastCommitFiles()
 end
 
 M.openDirtyFiles = function()
